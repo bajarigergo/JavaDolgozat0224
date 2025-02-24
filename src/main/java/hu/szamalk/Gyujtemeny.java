b9bc994a1255b50980f9b75ed47a11029904132f
bajarigergo <gergob10@gmail.com>
2025-02-24 10:55:32 +0100

    Getterek es setterek Konyv és Gyujtemeny osztályban. Rendezes Konyv osztalyban

--- src/main/java/hu/szamalk/Gyujtemeny.java
+++ src/main/java/hu/szamalk/Gyujtemeny.java
@@ -1,13 +1,15 @@
 package hu.szamalk;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Objects;
 
-public abstract class Gyujtemeny implements Iterable {
+public abstract class Gyujtemeny implements Iterable{
     private int ferohely;
     private ArrayList<Media> gyujtemeny = new ArrayList<>(5);
 
     public Gyujtemeny(ArrayList<Media> gyujtemeny) {
-        this.gyujtemeny = gyujtemeny;
+        setGyujtemeny(gyujtemeny);
         ferohely= gyujtemeny.size();
     }
     public void vetel(Media media){
@@ -19,5 +21,32 @@ public abstract class Gyujtemeny implements Iterable {
     public void eladas(int index){
         gyujtemeny.remove(gyujtemeny.get(index));
         ferohely=gyujtemeny.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gyujtemeny that = (Gyujtemeny) o;
+        return Objects.equals(gyujtemeny, that.gyujtemeny);
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+
+    public void setGyujtemeny(ArrayList<Media> gyujtemeny) {
+        if (gyujtemeny.size()<6){
+            this.gyujtemeny = gyujtemeny;
+        } else {
+            this.gyujtemeny = new ArrayList<>();
+            //throw new RuntimeException("Túl sok média van a gyüjteményben.");
+        }
+    }
+
+    public ArrayList<Media> getGyujtemeny() {
+        return new ArrayList<>(gyujtemeny);
     }
 }
--- src/main/java/hu/szamalk/Konyv.java
+++ src/main/java/hu/szamalk/Konyv.java
@@ -1,5 +1,7 @@
 package hu.szamalk;
 
+import java.text.Collator;
+
 public class Konyv {
     private String cim;
     private String szerzo;
@@ -15,6 +17,30 @@ public class Konyv {
         this.kiadasiEv = kiadasiEv;
     }
 
+    public String getCim() {
+        return cim;
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public int getKiadasiEv() {
+        return kiadasiEv;
+    }
+
+    public int cimRendez(Konyv o1, Konyv o2) {
+        Collator collator = Collator.getInstance();
+        return collator.compare(o1.getCim(),o2.getCim());
+    }
+    public int szerzoRendez(Konyv o1, Konyv o2) {
+        Collator collator = Collator.getInstance();
+        return collator.compare(o1.getSzerzo(),o2.getSzerzo());
+    }
+    public int kiadasRendez(Konyv o1, Konyv o2) {
+        Collator collator = Collator.getInstance();
+        return collator.compare(o1.getKiadasiEv(),o2.getKiadasiEv());
+    }
 }
 
 
